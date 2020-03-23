3508629b00e0f491a33bfedee906790019e8d441
Mateusz Łukasiński <mat.lukasinski@gmail.com>
2020-03-23 19:51:56 +0100

    Initial Commit

---
+++ Busex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Busex
+//
+//  Created by Mateusz Łukasiński on 23/03/2020.
+//  Copyright © 2020 AIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
